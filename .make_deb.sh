b69aab51735e37dc87671b00d2ae1601f0fdcd58
ob-yalian <luotao2025@163.com>
2026-08-07 17:05:12 +0800

    Trimmed the branch to only the orbbec_camera sub directory

--- .make_deb.sh
+++ .make_deb.sh
@@ -1,13 +1,21 @@
 #!/usr/bin/bash
 set -e
 CURR_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd -P)"
-sub_dir=(
-  orbbec_camera_msgs
-  orbbec_camera
-)
+rm -fr debian obj-x86_64-linux-gnu .obj-aarch64-linux-gnu || true
+export DEBIAN_INC=1
+export BUILDING_PACKAGE=1
+bloom-generate rosdebian --os-name ubuntu --ros-distro $ROS_DISTRO -i ${DEBIAN_INC}.$(date +%Y%m%d.%H%M%S)
+sed -i 's/dh $@/dh $@ --parallel/g' debian/rules
 
-for pkg in "${sub_dir[@]}"; do
-  cd "${pkg}"
-  bash .make_deb.sh
-  cd "${CURR_DIR}"
-done
+sed -i '/^override_dh_shlibdeps:/,/^$/d' debian/rules
+
+cat >> debian/rules << 'EOF'
+
+override_dh_shlibdeps:
+	dh_shlibdeps --dpkg-shlibdeps-params=--ignore-missing-info
+EOF
+
+PARALLEL=$(nproc)
+if [ "$PARALLEL" -gt 3 ]; then PARALLEL=3; fi
+export DEB_BUILD_OPTIONS="parallel=$PARALLEL"
+fakeroot debian/rules binary
